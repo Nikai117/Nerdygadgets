3a902a7d1eda1c465d78984aa02cc3cc3ea8409b
Jarito404 <jamesvtende@gmail.com>
2023-10-28 15:29:40 +0200

    Remove products klaar

--- winkelmand.php
+++ winkelmand.php
@@ -1,20 +1,42 @@
 <?php
 //onze session array heet "winkelmand". dus je roept het bijvoorbeeld zo op: $_SESSION['winkelmand'][0][0]['UnitPrice']
-include __DIR__ . "/header.php";
+include __DIR__ . "/header.php";       
 
+function checkEmpty($arr) {
+    if(!isset($arr[0]) || $arr[0] == NULL) {
+        print ("Yarr, de winkelmand is leeg."); 
+        return true;//het eerste element is leeg -> de hele winkelmand is leeg
+    }
 
-if(!isset($_SESSION['winkelmand'][0]) || $_SESSION['winkelmand'][0] == NULL) {//het eerste element is leeg -> de hele winkelmand is leeg
-    print ("Yarr, de winkelmand is leeg.");
-} else {
-    #hieronder zijn gewoon printjes voor testen
-    foreach($_SESSION['winkelmand'] as $item) {
-        print_r($item[0]['UnitPrice']);
+    return false; //eerste element is niet null 
+}
+
+if(!checkEmpty($_SESSION['winkelmand'])) {
+    if(isset($_POST['action'])) {
+        $action = explode(" ", $_POST['action']);
+        if($action[0] == "remove") {//mogelijke functionaliteit om via de winkelwagen producten toe te voegen  
+            unset($_SESSION['winkelmand'][0][$action[1]]);//$action[1] is de index van het product in de session array
+            array_splice($_SESSION['winkelmand'], $action[1], 1);//om de NULL row te verwijderen die je krijgt van unset()
+            
+            
+            header("Location: winkelmand.php");//refresh zodat de user          
+        }
+    }
+
+    for($i = 0; $i < count($_SESSION['winkelmand']); $i++) {
+        print_r($_SESSION['winkelmand'][$i][0]);
+        print('
+        <form method="post" action="">
+            <button type="submit" name="action" value="remove '. $i.'">Verwijder</button>
+        </form>
+        ');//ik gebruik remove in value, zodat we wellicht later nog een andere functionaliteit kunnen toevoegen: toevoegen via de winkelmand - James
         print("<br>");
     }
 
-    print_r($_SESSION['winkelmand'][0][0]);
+    #hieronder zijn gewoon printjes voor testen
+    //print_r($_SESSION['winkelmand'][0][0]);
 
-    var_dump($_SESSION['winkelmand']);
+
     #einde
 }
 ?>
